d63875730de6e2086e5cc393c0214de2c8c1080d
amin19 <amin.naddaf@sjsu.edu>
2022-03-16 13:51:14 -0700

    Added randomizer code

--- sketch.js
+++ sketch.js
@@ -1,7 +1,18 @@
+let dogs =["Kemp","Scott","Nelson","Pham","Kat"];
 function setup() {
-  createCanvas(400, 400);
-}
-
-function draw() {
+  createCanvas(600, 600);
   background(220);
+
+console.log("inital array is";
+console.log(dogs);
+
+dogs.splice();
+console.log("array after splice")
+console.log(dogs);
+
+dogs.unshift("Abhams");
+console.log("array after unshift")
+console.log(dogs);
+}
+function draw() {
 }
